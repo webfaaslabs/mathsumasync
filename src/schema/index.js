a847c4ff4a31182538e52727b88c2ae148b1930b
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2019-11-02 12:25:35 -0300

    init

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,10 +1,12 @@
 "use strict";
 
+module.exports.format = "json-schema";
+
 module.exports.input = {
     type: "array",
     items: {type: "number"}
-}
+};
 
 module.exports.output = {
     type: "number"
-}
+};
